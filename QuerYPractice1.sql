6693377c21a75a2c16300c9508531e8af680651b
Shasna-h <shemwetahazina@gmail.com>
2024-12-23 12:09:32 +0300

    first commit

---
+++ QuerYPractice1.sql
@@ -0,0 +1,7 @@
+SELECT TOP (1000) [BusinessEntityID]
+      ,[DepartmentID]
+      ,[ShiftID]
+      ,[StartDate]
+      ,[EndDate]
+      ,[ModifiedDate]
+  FROM [AdventureWorks2019].[HumanResources].[EmployeeDepartmentHistory]
